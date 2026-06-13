92dafd70e4f23aa8e03a10b635ae7e50e6807a91
Mahfuzur Rahman <kali@kali.kali>
2026-06-13 21:25:17 +0600

    Variables learned

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 123456789;
+let name = "Mahfuz"
+var email = "anything@example.com"
+accountCity = "Dhaka"
+
+name = "Idiot"
+email = "idiot@example.com"
+accountCity = "Idiotville"
+console.log(name, email, accountID, accountCity) // multiple variables can be printed by separating them comma
+
+let accountState;
+console.log(accountState) // undefined
+
+//console.table({name, email, accountID})
