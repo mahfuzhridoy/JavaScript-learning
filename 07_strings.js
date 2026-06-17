541d62b2c4763cc8fb0d4daaede4c47f6f79c962
Mahfuzur Rahman <mahfuz291199@gmail.com>
2026-06-17 06:12:23 +0600

    strings

---
+++ 07_strings.js
@@ -0,0 +1,4 @@
+//strings are most commonly used data type in programming. They are used to represent text and can be created using single quotes, double quotes, or backticks in JavaScript.
+
+// for example: 
+let str1 = 'Hello, World!'; // using single quotes
