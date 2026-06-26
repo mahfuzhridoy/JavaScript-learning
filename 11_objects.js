845c57b7df8934f790f7b63204561775eac63a9a
Mahfuzur Rahman <mahfuz291199@gmail.com>
2026-06-26 23:54:54 +0600

    Object part 2 Practiced

--- 11_objects.js
+++ 11_objects.js
@@ -1,3 +1,6 @@
+
+/*
+
 // objects are non-primitive(reference) data type. this is most used data type in JS.
 
 // object literals
@@ -22,14 +25,14 @@ const jsUser = {
 } 
 
 
-/*    
+    
 console.log(jsUser.isLoggedin)  // We can access the properties by dot (.) operator. there are other ways to access. For example:
 
 console.log(jsUser["age"]) // the parameter is passed like string becaus keys are processed and stored as string in objects. we need to know this method because if we declare the key in string format, we can not access the key property with dot operator. For example:
 
 //console.log(jsUser."full name")  // this line will throw an error. correct way to access this key is using a square bracket. like above example.
 
-*/
+
 
 
 
@@ -45,7 +48,7 @@ const anotherObject = {
 
 }
 
-/*
+
 
 console.log(anotherObject[mySym])
 console.log(anotherObject.name)
@@ -66,7 +69,7 @@ console.log(anotherObject.name)
 anotherObject.name = "hamba" // the change wont be written into object.
 console.log(anotherObject.name) // output: Mahfuzur rahman. because the object is freezed.
 
-*/
+
 
 anotherObject.greetings = function greet(){
     console.log("Hello Mahfuz")
@@ -91,6 +94,88 @@ anotherObject.greeting2 = function greeting2(){
 console.log(anotherObject)
 console.log(anotherObject.greeting2())
 
+*/
+
+const { use } = require("react")
 
 
 
+//++++++++++++++++++++++++++++++ Objects part 2 +++++++++++++++++++++++++++++++++++
+
+
+
+
+// Singleton
+
+
+const tinderUser = new Object() /// This creates a singleton object
+
+tinderUser.id = "1234"
+tinderUser.name = "Mahfuz"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+
+const regularUser = {
+    email : "any@gmail.com",
+    fullName : {
+        userFullName : {
+            firstName : "Mahfuzur",
+            lastName : "Rahman",
+
+            greet(){
+                console.log("Hello Guys")
+            }
+        }
+
+    }
+}  // this creates a literal object. This example also shows nesting of objects. we can create an object inside another object.
+
+regularUser.fullName.userFullName.surname = "Hridoy"
+
+//console.log(regularUser.fullName.userFullName.surname) // we cn access the property of the objects like this. or
+//console.log(regularUser ["fullName"]["userFullName"]["surname"]) // this is another way to access the object property.
+
+//  [Note: always use double quotes while specifyig a key insize a square bracket. if the key is a symbol then do not use double quotes !!!. Always be cautious about using comma]
+
+regularUser.fullName.userFullName.greet(); //accessing the method inside object. this line executes the code inside the greet method.
+
+
+//console.log(regularUser.fullNam?.userFullName.firstName) /// Question mark can be used to identify if the key actually exists in the object or not. this is used as alternate of if else conditions and makes it easy. tis syntax usually used in API.
+
+/// Merging objects
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+//const obj3 = {obj1, obj2} // inserts the second object into first object as an eleent
+//console.log(obj3)
+
+
+const obj4 = Object.assign({}, obj1, obj2)  // it is always a best practice to enter an empty object ad target. because assign method returns a new object by injecting the sources into the target
+console.log(obj4)
+
+
+// Another method of combining or merging with spread operator
+
+const obj5 = {...obj1, ...obj2, ...obj4}
+console.log(obj5)
+
+
+
+/// Accessing from the database. 
+
+// usually the data from database comes as the array of objects like below given example:
+
+const user = [
+    {
+        id: "1",
+        name: "Monir"
+    },
+
+    {
+        email: "anything@gmail.com",
+        password: "whatever"
+    }
+]
+user[0].id
