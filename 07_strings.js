6bb8c57c9e9569237cf08fd702b4172e0740bc78
Mahfuzur Rahman <mahfuz291199@gmail.com>
2026-07-27 11:02:01 +0600

    String revised

--- 07_strings.js
+++ 07_strings.js
@@ -3,11 +3,11 @@
 // for example: 
 let str1 = 'Hello, World!'; // using single quotes
 
-console.log(`I want to say say ${str1}`); // using backticks for string interpolation
+// console.log(`I want to say say ${str1}`); // using backticks for string interpolation
 
-// const gameName = new String('Find the goat');
+const gameName = new String('Find the goat');
 // 
-const gameName = `Find the goat`;
+// const gameName = `Find the goat`;
 // using String constructor. this stores word as key value pair. it is not recommended to use String constructor to create strings in JavaScript. it stores index as key and character as value. mostly used for type of applications like games where we need to access individual characters of the string.
 
 
@@ -39,8 +39,23 @@ const gameName = `Find the goat`;
 // //3. console.log(gameName.substring(0, 4)); // output: Find. it returns the part of the string between the start and end index. the end index is not included in the result.
 
 const newString = gameName.substring(3, 4); // if we add negative index in slice method, it counts from the end of the string. so -1 is the last character of the string, -2 is the second last character of the string and so on. in this case, -4 is the fourth last character of the string which is 'g' and 8 is the index of the character 't'. so it returns 'goat'.
-console.log(newString);
+// console.log(newString);
 
 
 
-// 4. 
+const anotherString = gameName.slice(-13, 4) // Starts froma  negative valu which is converted to gamename.length - 13 => starting index
+// console.log(anotherString)
+
+const newStringone = "  Hello World! "
+console.log(newStringone.trim()) // removing empty spaces from the string
+
+// replace() method replaces a character of a string
+
+console.log(newStringone.replace("World", "Mahfuz"));
+
+console.log(newStringone.includes("M")); // returned false because original string was not changed in previous operation
+
+console.log(newStringone.split(" ")); //splits the string based on the delimiter provided inside method and returns an array of the values
+
+
+
